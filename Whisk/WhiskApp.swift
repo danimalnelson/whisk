a9e7b18ddb1f298d9b3a2aa8a522c443420935b3
Dan Nelson <dannelson@icloud.com>
2025-08-04 21:29:16 -0500

    Initial Commit

---
+++ Whisk/WhiskApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhiskApp.swift
+//  Whisk
+//
+//  Created by Dan Nelson on 8/4/25.
+//
+
+import SwiftUI
+
+@main
+struct WhiskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
